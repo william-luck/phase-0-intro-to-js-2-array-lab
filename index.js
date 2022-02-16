f51c748302ded2e0e33bccef7cef8275ea0841ab
William Luck <luck.william.h@gmail.com>
2022-02-16 15:58:59 -0600

    commit tree c38684d181fcde7b39d044ee6f383c0c7e1f1a44

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Write your solution here!
+const cats = [
+    "Milo", 
+    "Otis", 
+    "Garfield"
+]
+destructivelyAppendCat("Ralph");
+cats.pop();
+destructivelyPrependCat("Bob");
+cats.shift();
+destructivelyRemoveLastCat();
+destructivelyAppendCat("Garfield");
+destructivelyRemoveFirstCat();
+destructivelyPrependCat("Milo");
+// done with first five tests, resets to original array each time
+
+const copyOfCats = appendCat("Broom"); // makes a copy of array with Broom at the end
+prependCat("cat name")
+removeLastCat();
+removeFirstCat();
+
+// removes first cat, returns new array, non-destructive
+function removeFirstCat() {
+    return cats.slice(1, cats.length);
+}
+
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyAppendCat(name) {
+   cats.push(name);
+}
+
+
+
+console.log(cats);
+console.log(copyOfCats); 
+
